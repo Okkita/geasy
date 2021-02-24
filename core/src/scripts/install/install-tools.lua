a483589b9e561dae39eb5eb507168f453c09e7cd
Oscar López Lacalle <okkita@gmail.com>
2021-02-25 00:18:02 +0100

    Adds prettier install and about strings

--- core/src/scripts/install/install-tools.lua
+++ core/src/scripts/install/install-tools.lua
@@ -3,28 +3,46 @@ Geasy.version = "0.1"
 
 function Geasy.reloadPackage()
     Geasy.uninstall()
-    cecho("<gold>:: Installing Geasy.\n")
+    cecho("<purple>:: Installing Geasy.\n")
     installPackage("D:/Geasy/core/build/Geasy-Core-" .. Geasy.version ..
                        ".mpackage")
 end
 
 function Geasy.uninstall()
-    cecho("<gold>:: Un-installing Geasy.\n")
+    cecho("<purple>:: Un-installing Geasy.\n")
     uninstallPackage("Geasy-Core-" .. Geasy.version)
 end
 
 function Geasy.about()
-    cecho("\n<gold>:: Geasy, version " .. Geasy.version .. "\n")
-    cecho("<gold>:: Geasy is maintained at ")
-    echoLink("https://github.com/Okkita/geasy/",
+    cecho("\n<purple>:: Geasy, version " .. Geasy.version .. "\n")
+    cecho("<purple>:: Geasy is maintained at ")
+    echoLink("https://github.com/Okkita/geasy/\n",
              "openUrl('https://github.com/Okkita/geasy/')",
              "https://github.com/Okkita/geasy/")
-    echo("\n")
+    cecho("<purple>:: '")
+    echoLink("ghelp", [[Geasy.help()]], "ghelp")
+    cecho("' <purple>for script commands\n")
 end
 
 function Geasy.installMessage(_, name)
     if not string.find(name, "Geasy") then return end
     Geasy.about()
+end
+
+function Geasy.help()
+    cecho("<purple>" .. [[
+
+    Geasy commands:
+    ghelp - this help file
+    gabout - info about this package
+
+    <purple>Debug:
+    gmcp - shows the gmcp data table
+
+    <red>Danger zone:
+    guninstall confirm - uninstalls this package
+
+]])
 end
 
 registerAnonymousEventHandler("sysInstallPackage", "Geasy.installMessage")
